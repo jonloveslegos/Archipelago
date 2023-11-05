5414e4b7bcd5a7cda02428ca0aa6e18de2b28437
jonloveslegos <jonloveslegos@gmail.com>
2023-11-05 11:00:16 -0500

    Fixed logic for True Lab Key

--- worlds/undertale/Rules.py
+++ worlds/undertale/Rules.py
@@ -141,8 +141,7 @@ def set_rules(multiworld: MultiWorld, player: int):
         set_rule(multiworld.get_location("Diary 6", player),
                  lambda state: state.can_reach("Waterfall", "Region", player) and state.has("Mystery Key", player, 1))
         set_rule(multiworld.get_location("True Lab Key", player),
-                 lambda state: state.can_reach("New Home", "Region", player)
-                               and state.can_reach("Letter Quest", "Location", player))
+                 lambda state: state.can_reach("Alphys Date", "Region", player))
         set_rule(multiworld.get_location("Chisps Machine", player),
                  lambda state: state.can_reach("True Lab", "Region", player))
         set_rule(multiworld.get_location("Donut Sale", player),
