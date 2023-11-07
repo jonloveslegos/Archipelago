bf89b484af189391a2be9390f787af2d91bd546f
jonloveslegos <jonloveslegos@gmail.com>
2023-11-07 12:54:12 -0500

    encounter randomizer basics

--- worlds/undertale/__init__.py
+++ worlds/undertale/__init__.py
@@ -55,6 +55,8 @@ class UndertaleWorld(World):
 
     data_version = 7
 
+    encounter_rando = {id: id for id in [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 28, 30, 31, 32, 33, 34, 35, 36, 40, 41, 42, 43, 50, 51, 52, 53, 54, 55, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 77, 78, 79, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134]}
+
     def _get_undertale_data(self):
         return {
             "world_seed": self.multiworld.per_slot_randoms[self.player].getrandbits(32),
@@ -232,6 +234,11 @@ class UndertaleWorld(World):
 
         self.multiworld.itempool += itempool
 
+        encounter_possibilities = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 28, 30, 31, 32, 33, 34, 35, 36, 40, 41, 42, 43, 50, 51, 52, 53, 54, 55, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 77, 78, 79, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, ]
+        self.random.shuffle(encounter_possibilities)
+        for id, item in self.encounter_rando:
+            self.encounter_rando[id] = encounter_possibilities.pop()
+
     def set_rules(self):
         set_rules(self.multiworld, self.player)
         set_completion_rules(self.multiworld, self.player)
@@ -294,6 +301,8 @@ class UndertaleWorld(World):
                 slot_data[option_name] = int(option.value)
             elif slot_data.get(option_name, None) is None and type(option.value) in {str, int}:
                 slot_data[option_name] = int(option.value)
+        slot_data["Encounter Rando"] = self.encounter_rando
+
         return slot_data
 
     def create_item(self, name: str) -> Item:
