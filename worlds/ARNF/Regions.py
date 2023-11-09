afd629b05f65050b43fed1c6ed8960c677f5e100
BetaSprite <87193968+BetaSprite@users.noreply.github.com>
2023-11-08 21:08:01 -0600

    Create Regions.py

---
+++ worlds/ARNF/Regions.py
@@ -0,0 +1,9 @@
+from typing import Dict, List, NamedTuple
+
+class ARNFRegionData(NamedTuple):
+    connecting_regions: List[str] = []
+
+region_data_table: Dict[str, ARNFRegionData] = {
+    "Menu": ARNFRegionData(["HTRAE"]),
+    "HTRAE": ARNFRegionData(),
+}
