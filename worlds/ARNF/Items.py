44f54ee22db5c4a20d0f713f1423e03c9049d032
BetaSprite <87193968+BetaSprite@users.noreply.github.com>
2023-11-19 12:02:49 -0600

    Update Items.py

--- worlds/ARNF/Items.py
+++ worlds/ARNF/Items.py
@@ -1,4 +1,22 @@
+from typing import Callable, Dict, NamedTuple, Optional
 from BaseClasses import Item
+from .Options import ItemWeights
 
 class ARNFItem(Item):
     game: str = "A Robot Named Fight!"
+
+item_table: Dict[str, int] = {
+    "NonProgressionItem": 1,
+    "ProgressionItem":    2
+}
+
+default_weights: Dict[str, int] = {
+    "NonProgressionItem":    1,
+    "ProgressionItem":       0
+}
+
+item_pool_weights: Dict[int, Dict[str, int]] = {
+    ItemWeights.option_default:     default_weights
+}
+
+lookup_id_to_name: Dict[int, str] = {id: name for name, id in item_table.items()}
