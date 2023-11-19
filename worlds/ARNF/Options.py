9b38d6ebc40bf8bdc4bb3d8ed15304fa7260312c
BetaSprite <87193968+BetaSprite@users.noreply.github.com>
2023-11-19 12:03:07 -0600

    Update Options.py

--- worlds/ARNF/Options.py
+++ worlds/ARNF/Options.py
@@ -1,6 +1,21 @@
 from dataclasses import dataclass
 from Options import Toggle, DefaultOnToggle, DeathLink, Range, Choice, PerGameCommonOptions
 
+class TotalLocations(Range):
+    """Number of pickups that should be considered for checks.  Please note that approximately 47 non-progress items and 3 progress items are in each run of the game."""
+    display_name = "Total Locations"
+    range_start = 20
+    range_end = 250
+    default = 47
+
+class ItemWeights(Choice):
+    """Set item_pool_presets to true if you want to use one of these presets.
+    Preset choices for determining the weights of the item pool."""
+    display_name = "Item Weights"
+    option_default = 0
+
 @dataclass
 class ARNFOptions(PerGameCommonOptions):
+    total_locations: TotalLocations
+    item_weights: ItemWeights
     death_link: DeathLink
