0213afe0808d5b3198292b3eef46d62778bd20e0
BetaSprite <87193968+BetaSprite@users.noreply.github.com>
2023-12-05 16:09:21 -0600

    Update Locations.py

--- worlds/ARNF/Locations.py
+++ worlds/ARNF/Locations.py
@@ -4,8 +4,13 @@ from typing import Callable, Dict, NamedTuple, Optional
 from BaseClasses import Location
 from .Options import TotalLocations
 
+
 class ARNFLocation(Location):
     game: str = "A Robot Named Fight!"
+
+
+arnf_locations_start_id = 7331000
+
 
 def get_ordered_item_pickups(n: int) -> Dict[str, int]:
     """Get n ItemPickups, capped at the max value for TotalLocations"""
@@ -13,9 +18,9 @@ def get_ordered_item_pickups(n: int) -> Dict[str, int]:
     n = min(n, TotalLocations.range_end)
     logger = logging.getLogger()
     logger.info(f'TotalLocations.range_end {TotalLocations.range_end}, max {max(n, 0)}, min {min(n, TotalLocations.range_end)}')
-    return { f"ARNF{i+1}": i for i in range(n) }
+    return { f"ARNF{i+1}": arnf_locations_start_id+i for i in range(n) }
+
 
 item_pickups = get_ordered_item_pickups(TotalLocations.range_end)
 location_table = item_pickups
-
 lookup_id_to_name: Dict[int, str] = {id: name for name, id in location_table.items()}
